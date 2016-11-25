936528e1b890020f34734606a1327d62ff3f8bba
c1wei <sjcastroe@gmail.com>
2016-11-25 02:07:08 -0800

    Comment connection.js

--- connection.js
+++ connection.js
@@ -1,14 +1,17 @@
-/*******************************************************************
- * Create a connection to a mysql database using the mysql module. *
- *******************************************************************/
+/***************************************************************************
+ * Create a connection to a mysql database using the node.js mysql client. *
+ ***************************************************************************/
 
 //Include the mysql module
 var mysql = require('mysql');
 
 function Connection() {
+    /** @var {Pool} A connection pool. A cache of database connections that makes it easy to share connections between requests.*/
     this.pool = null;
 
-    //Create a connection pool
+    /**
+     * Constructs a Connection object by initializing the internal pool variable.
+     */
     this.init = function() {
         this.pool = mysql.createPool({
             connectionLimit: 10, //Limit the maximum connections to 10
@@ -19,6 +22,10 @@ function Connection() {
         });
     };
 
+    /**
+     * Acquires a connection from the pool
+     * @param  {Function} callback Connection handler. Should release the connection after it is done using it.
+     */
     this.acquire = function(callback) {
         this.pool.getConnection(function(err, connection) {
             callback(err, connection);
